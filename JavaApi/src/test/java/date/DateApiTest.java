4d04b3e73801b939ec02cd8ebd61e2b0c6e0da69
bae <infoqoch@gmail.com>
2021-11-24 22:43:08 +0900

    date api  연습

---
+++ JavaApi/src/test/java/date/DateApiTest.java
@@ -0,0 +1,58 @@
+package date;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class DateApiTest {
+    @Test
+    void legacyDateApi_threadSafety(){
+        Date date = new Date();
+        long time = date.getTime();
+        System.out.println(time); // Date 인데  time 이 나오고 time 은 이상한 long 이...
+
+        Date now = new Date();
+        now.setTime(335983742343245l); // mutable 함. 그러니까 인스턴스의 값이 가변적임. Thread Safety 하지 아니함.
+        System.out.println(now);
+    }
+
+    @Test
+    void legacyDateApi_DateTypeSafety(){
+        GregorianCalendar calendar = new GregorianCalendar(2021, 99, 23); // month 가 5를 넣지만 6월이다. int를 99 로 넣을 수 있다.
+        System.out.println(calendar.getTime());
+    }
+
+    @Test
+    void 새로운_데이트_타입의_대두(){
+        Instant instant = Instant.now(); // 컴퓨터용 시간
+        LocalDate localDate = LocalDate.now(); // 이하 사람 용
+        LocalDateTime localDateTime = LocalDateTime.now(); // OS의 기준에 따라... 만약 AWS 등 해외 서버를 사용한다면 위험할수도?
+        ZonedDateTime zonedDateTime = ZonedDateTime.now(); // 설정에 따라... 안전할지도?
+    }
+
+    @Test
+    void 연습하기(){
+        LocalDateTime localDateTime = LocalDateTime.of(2021, Month.JULY,7, 10,15);
+        System.out.println(localDateTime);
+        System.out.println(localDateTime.atZone(ZoneId.of("Asia/Seoul")));
+
+        LocalDateTime now = LocalDateTime.now();
+
+        Duration between = Duration.between(now, localDateTime);
+        System.out.println(between.toDays());
+
+        Period period = LocalDate.now().until(LocalDate.of(2022, Month.JULY, 5));
+        System.out.println(period.get(ChronoUnit.DAYS));
+    }
+
+    @Test
+    void formatter(){
+        DateTimeFormatter MMddyyyy = DateTimeFormatter.ofPattern("MM-dd-yyyy");
+        String format = LocalDate.now().format(MMddyyyy);
+        System.out.println(format);
+    }
+}
