196e5a5ab61acbb7b0322ff6809d1e1ed5b7b853
bae <infoqoch@gmail.com>
2022-05-14 23:36:08 +0900

    리스코프 원칙 2, 오버라이딩으로 인한 치환 불가(area 값이 다름)

--- classAndInstance/src/main/java/liskov/Square.java
+++ classAndInstance/src/main/java/liskov/Square.java
@@ -1,4 +1,17 @@
 package liskov;
 
 public class Square extends Rectangle {
+    @Override
+    public void setHeight(int height) {
+        setSide(height);
+    }
+    @Override
+    public void setWidth(int width) {
+        setSide(width);
+    }
+
+    public void setSide(int size) {
+        super.setHeight(size);
+        super.setWidth(size);
+    }
 }
--- classAndInstance/src/test/java/liskov/RectangleTest.java
+++ classAndInstance/src/test/java/liskov/RectangleTest.java
@@ -8,9 +8,17 @@ import static org.junit.jupiter.api.Assertions.*;
 class RectangleTest {
 
     @Test
-    void test() {
+    void test1() {
         Rectangle rec = new Square();
         rec.setWidth(10);
         assertThat(rec.area()).isEqualTo(100);
+    }
+
+    @Test
+    void test2() {
+        Rectangle rec = new Square();
+        rec.setWidth(10);
+        rec.setHeight(20);
+        assertThat(rec.area()).isEqualTo(200);
     }
 }
