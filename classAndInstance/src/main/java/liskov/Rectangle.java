7a05b1319e33b2702cf5eacd92041883ee0f2138
bae <infoqoch@gmail.com>
2022-05-14 23:26:54 +0900

    리스코프 원칙 1

---
+++ classAndInstance/src/main/java/liskov/Rectangle.java
@@ -0,0 +1,26 @@
+package liskov;
+
+public class Rectangle {
+    private int height;
+    private int width;
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int area(){
+        return height*width;
+    }
+}
